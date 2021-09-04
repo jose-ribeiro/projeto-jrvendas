16e1eeafcdf4c20d3331acc84e9e636ac4129b3d
jose-ribeiro <57302604+jose-ribeiro@users.noreply.github.com>
2021-09-04 08:15:58 -0300

    Projeto criado

---
+++ backend/src/main/java/com/jrservice/jrvendas/JrvendasApplication.java
@@ -0,0 +1,13 @@
+package com.jrservice.jrvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jrservice/jrvendas/JrvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrservice.jrvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
