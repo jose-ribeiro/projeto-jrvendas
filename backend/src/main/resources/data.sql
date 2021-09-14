660fc218a0241a23999c141e41fbe3989cae800a
jose-ribeiro <57302604+jose-ribeiro@users.noreply.github.com>
2021-09-14 11:45:08 -0300

    Implantação no heroku

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,24 +1,52 @@
-INSERT INTO tb_cidades(name) VALUES ('Careiro');
-INSERT INTO tb_cidades(name) VALUES ('Autazes');
-INSERT INTO tb_cidades(name) VALUES ('Manacapuru');
+INSERT INTO tb_cidades(name) VALUES ('Iranduba');
 INSERT INTO tb_cidades(name) VALUES ('Parintins');
 INSERT INTO tb_cidades(name) VALUES ('Tefe');
-INSERT INTO tb_cidades(name) VALUES ('Iranduba');
-INSERT INTO tb_cidades(name) VALUES ('Rio preto');
-INSERT INTO tb_cidades(name) VALUES ('Nova Olinda');
+INSERT INTO tb_cidades(name) VALUES ('Nova Olinda do Norte');
+INSERT INTO tb_cidades(name) VALUES ('Careiro Castanho');
+INSERT INTO tb_cidades(name) VALUES ('Urucurituba');
+INSERT INTO tb_cidades(name) VALUES ('Caracarai');
+INSERT INTO tb_cidades(name) VALUES ('Maues');
+INSERT INTO tb_cidades(name) VALUES ('Barrerinha');
+INSERT INTO tb_cidades(name) VALUES ('Santo Antonio do Iça');
+INSERT INTO tb_cidades(name) VALUES ('Nhamunda');
+INSERT INTO tb_cidades(name) VALUES ('Novo Aripuanã');
+INSERT INTO tb_cidades(name) VALUES ('Boa vista do Ramos');
+INSERT INTO tb_cidades(name) VALUES ('Tabatinga');
+INSERT INTO tb_cidades(name) VALUES ('Manicore');
+INSERT INTO tb_cidades(name) VALUES ('Labrea');
+INSERT INTO tb_cidades(name) VALUES ('São Sebartião do Utumã');
+INSERT INTO tb_cidades(name) VALUES ('Humaita');
+INSERT INTO tb_cidades(name) VALUES ('São Paulo de Olivença');
+INSERT INTO tb_cidades(name) VALUES ('Benjamim Constan');
+INSERT INTO tb_cidades(name) VALUES ('Tonantins');
 INSERT INTO tb_cidades(name) VALUES ('Coari');
-INSERT INTO tb_cidades(name) VALUES ('Manaquiri');
 
-INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (1,81,6611.0,'2021-04-01');
-INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (2,83,1211.0,'2021-04-01');
-INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (3,83,1455.0,'2021-04-01');
-INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (4,83,1234.0,'2021-04-01');
-INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (5,83,5501.0,'2021-04-01');
-INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (6,83,5501.0,'2021-04-01');
-INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (7,83,5501.0,'2021-04-01');
-INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (8,83,5501.0,'2021-04-01');
-INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (9,83,5501.0,'2021-04-01');
-INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (10,83,5501.0,'2021-04-01')
+
+
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (1,1127,548688.79,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (2,945,698238.26,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (3,878,740545.61,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (4,877,313717.37,'2021-08-21');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (5,789,373103.92,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (6,743,151139.68,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (7,461,212842.56,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (8,425,328329.76,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (9,418,184404.35,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (10,389,86244.40,'2021-08-21');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (11,362,253643.93,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (12,350,136319.91,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (13,343,213280.80,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (14,306,64791.60,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (15,299,200804.23,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (16,232,154949.41,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (17,220,118559.31,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (18,185,120370.99,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (19,167,45746.12,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (20,68,63383.49,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (21,66,19031.15,'2021-08-31');
+INSERT INTO tb_dados(cidade_id,atendimentos,valor,date) VALUES (22,58,362281.99,'2021-08-31');
+
+
 
 
 
